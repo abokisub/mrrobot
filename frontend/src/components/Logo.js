ae407f27acf803930eb577c55414c652682a7dd1
abokisub <abokisub@gmail.com>
2025-11-28 13:52:40 +0100

    feat: Replace SVG logo with custom logo.png image
    - Updated Logo component to use /assets/images/logo.png
    - Copied logo from frontend/assets/images to public/assets/images
    - Logo now displays globally across the application

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+Logo.propTypes = {
+  disabledLink: PropTypes.bool,
+  sx: PropTypes.object,
+};
+
+export default function Logo({ disabledLink = false, sx }) {
+  const logo = (
+    <Box 
+      component="img"
+      src="/assets/images/logo.png"
+      alt="KoboPoint Logo"
+      sx={{ 
+        width: 40, 
+        height: 40, 
+        objectFit: 'contain',
+        ...sx 
+      }}
+    />
+  );
+  
+  if (disabledLink) {
+    return <>{logo}</>;
+  }
+
+  return <RouterLink to="">{logo}</RouterLink>
+}
